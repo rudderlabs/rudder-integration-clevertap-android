f9a5b5c1957069ba8422bc00e8e2ea0bcc62f216
Abhishek Pandey <abhishekpandey10196@gmail.com>
2023-02-14 23:15:51 +0530

    chore: fix sample app running issue

--- sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
+++ sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
@@ -8,8 +8,8 @@ import com.rudderstack.android.sdk.core.RudderLogger
 
 class MainApplication : Application() {
     companion object {
-        private const val WRITE_KEY = "1qIlf7JB5aMuiQbOuJeb9vzXuWK"
-        private const val CONTROL_PLANE_URL = "https://c7d361b63320.ngrok.io"
+        private const val WRITE_KEY = "2LMFf1X4aYWSrE7m9rYsvUjJUO6"
+//        private const val CONTROL_PLANE_URL = "https://c7d361b63320.ngrok.io"
         lateinit var rudderClient: RudderClient
     }
 
@@ -19,10 +19,11 @@ class MainApplication : Application() {
             this,
             WRITE_KEY,
             RudderConfig.Builder()
-                .withControlPlaneUrl(CONTROL_PLANE_URL)
+//                .withControlPlaneUrl(CONTROL_PLANE_URL)
+                .withDataPlaneUrl("https://rudderstacbumvdrexzj.dataplane.rudderstack.com")
                 .withTrackLifecycleEvents(false)
                 .withFactory(CleverTapIntegrationFactory.FACTORY)
-                .withLogLevel(RudderLogger.RudderLogLevel.VERBOSE)
+                .withLogLevel(RudderLogger.RudderLogLevel.NONE)
                 .build()
         )
     }
